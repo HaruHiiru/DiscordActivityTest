334831f7ac458884df857553f88776cc06cfdbc2
Haru <96742478+HaruHiiru@users.noreply.github.com>
2024-05-10 19:01:49 +0100

    hhh

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    CLIENT_ID: "0000000000000000000", // Discord Application ID
-    COMPANY_NAME: "TheSleepyKoala", // Company or Developer Name
-    PRODUCT_NAME: "Just Another Generic Roguelike", // Game Name
+    CLIENT_ID: "1238522545238577282", // Discord Application ID
+    COMPANY_NAME: "No one really", // Company or Developer Name
+    PRODUCT_NAME: "L7ouma For Discord", // Game Name
     PRODUCT_VERSION: "0.1.0", // Game Version
 };
